3d1f7ef400eed19486883f3f3b45d1e62905b57d
dhruvki94 <dhruvki94@gmail.com>
2023-04-13 18:20:42 +0530

    Add qr url support

--- app/src/main/java/com/example/ticketing/model/impl/ScannerServiceImpl.kt
+++ app/src/main/java/com/example/ticketing/model/impl/ScannerServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.example.ticketing.model.impl
 
 import com.example.ticketing.model.service.ScannerService
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -18,7 +19,7 @@ constructor(
       scanner.startScan()
         .addOnSuccessListener {
           launch {
-            send(it.rawValue)
+            send(it.rawValue.toString().replace("/",""))
           }
         }.addOnFailureListener {
           it.printStackTrace()
